07c32554379da714554f2a156603ed6ea8d61e72
田浪 <1584731441@qq.com>
2022-02-26 13:16:47 +0800

    init project

---
+++ app.js
@@ -0,0 +1,48 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const log4js = require('log4js')
+const log = log4js.getLogger()
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+)
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(
+  views(__dirname + '/views', {
+    extension: 'pug',
+  })
+)
+
+// logger
+app.use(async (ctx, next) => {
+  await next()
+  log.level = 'debug'
+  log.debug('Some debug messages')
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+})
+
+module.exports = app
